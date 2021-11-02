1998906f9c939bc92d6c81672324480192014a47
Jean Carlos Calderon Vasquez <jeancaldron@gmail.com>
2021-11-01 20:26:02 -0400

    fix jest

--- homelike-service/src/config/testDB.ts
+++ homelike-service/src/config/testDB.ts
@@ -1,12 +1,8 @@
-// test/db.ts
 import mongoose from 'mongoose';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 
 const mongod = new MongoMemoryServer();
 
-/**
- * Connect to mock memory db.
- */
 export const connect = async () => {
     const uri = await mongod.getUri();
 
@@ -19,18 +15,12 @@ export const connect = async () => {
     await mongoose.connect(uri, mongooseOpts);
 }
 
-/**
- * Close db connection
- */
 export const closeDatabase = async () => {
     await mongoose.connection.dropDatabase();
     await mongoose.connection.close();
     await mongod.stop();
 }
 
-/**
- * Delete db collections
- */
 export const clearDatabase = async () => {
     const collections = mongoose.connection.collections;
 
